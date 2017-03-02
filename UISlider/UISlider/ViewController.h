1defe2504c5898dd358a367bd0952df0078d7670
hezhibo <2465384900@qq.com>
2017-03-02 11:19:52 +0800

    何志博
    UISlider学习

---
+++ UISlider/UISlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISlider
+//
+//  Created by 小何 on 17/3/2.
+//  Copyright © 2017年 zkbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UISlider/UISlider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISlider
+//
+//  Created by 小何 on 17/3/2.
+//  Copyright © 2017年 zkbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
